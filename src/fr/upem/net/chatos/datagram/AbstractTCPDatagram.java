c9eddea05a3e178cae5efc92010612201f2067a9
potetial <potetial@DESKTOP-5F6GRRT>
2021-04-07 14:36:24 +0200

    Oups (oublie de AbstractTCPDatagram)

---
+++ src/fr/upem/net/chatos/datagram/AbstractTCPDatagram.java
@@ -0,0 +1,55 @@
+package fr.upem.net.chatos.datagram;
+
+import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+abstract class AbstractTCPDatagram implements Datagram{
+	private final String sender;
+	private final String recipient;
+	private final short password;
+	
+	AbstractTCPDatagram(String sender, String recipient, short password) {
+		Objects.requireNonNull(sender);
+		Objects.requireNonNull(recipient);
+		this.sender = sender;
+		this.recipient = recipient;
+		this.password = password;
+	}
+	
+	public short getPassword() {
+		return password;
+	}
+	
+	public String getRecipient() {
+		return recipient;
+	}
+	
+	public String getSender() {
+		return sender;
+	}
+	
+	Optional<ByteBuffer> toByteBuffer(Logger logger, byte opCode) {
+		var bbSend = UTF8_CHARSET.encode(sender);
+		
+		if (bbSend.limit() > MAX_STRING_SIZE) {
+			logger.info("Login exceed the limit (1024), ignoring command");
+			return Optional.empty();
+		}
+		var bbRecipient = UTF8_CHARSET.encode(recipient);
+		if (bbRecipient.limit() > MAX_STRING_SIZE) {
+			logger.info("Recipient exceed the limit (1024), ignoring command");
+			return Optional.empty();
+		}
+		
+		return Optional.of(ByteBuffer.allocate(1+2*Short.BYTES + bbSend.limit() + bbRecipient.limit() + Short.BYTES)
+				.put(opCode)
+				.putShort((short)bbSend.limit())
+				.put(bbSend)
+				.putShort((short)bbRecipient.limit())
+				.put(bbRecipient)
+				.putShort(password)
+				.flip());
+	}
+}
